6679de36f2ca11a5351b1b350d11228bd5f0d801
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2023-11-23 23:48:41 +0000

    Removed notice icon

--- src/app/page.js
+++ src/app/page.js
@@ -33,22 +33,7 @@ export default function Home() {
 				{!newsletterFormDisabled && (
 					<NewsletterSignupForm htmlForId="hero_newsletter_signup_form" />
 				)}
-				<p className="lg:text-xl text-center bg-orange-300/20 px-5 py-2 rounded-full flex justify-center items-center gap-3">
-					<svg
-						fill="none"
-						stroke="currentColor"
-						strokeWidth={1.5}
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						className="w-8 hidden sm:block"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-						/>
-					</svg>
+				<p className="lg:text-xl text-center bg-orange-300/20 px-5 py-2 rounded-full">
 					This project is under active development and not publicly available.
 				</p>
 			</div>
